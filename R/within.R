da6ce9049637dcba7c7647eb39740f4c54607227
Michael Friendly <friendly@yorku.ca>
2018-12-14 09:43:12 -0500

    remove export for within

--- R/within.R
+++ R/within.R
@@ -1,15 +1,16 @@
 
 #' Select observations within a given range
 #'
+#' This function masks `base::within` and so is no longer exported. Eventually it will be removed.
+#' 
 #' @param x A vector 
 #' @param a Lower limit
 #' @param b Upper limit
 #'
 #' @return A logical vector of the same length as \code{x} 
-#' @export
 #'
 #' @examples
-#' within(1:10, 2, 5)
+#' WordPools:::within(1:10, 2, 5)
 #' 
 within <- function(x, a, b) {
   (!is.na(x)) & (x >= a) & (x <= b)
